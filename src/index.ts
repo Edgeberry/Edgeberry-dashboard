3ab5d26c6e1f8f6fe17c4e6d676d9b1ca0cb25e4
Tom Santens <tom.santens@gmail.com>
2024-04-08 19:25:05 +0200

    API: Thing list + Thing Shadow

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,7 @@ import express from 'express';
 import cors from 'cors';
 // API routes
 import userRoutes from './routes/user';
-// AWS SDK
-import { IoTClient, ListThingsCommand } from '@aws-sdk/client-iot';
+import thingRoutes from './routes/things';
 
 
 /* Express API server */
@@ -31,42 +30,6 @@ app.use(express.json());        // JSON API
 app.use(cors({origin:'*'}));    // Cross-origin references
 // Use the API Routers
 app.use('/api/user', userRoutes );
+app.use('/api/thing', thingRoutes );
 // Start the webserver
 app.listen( 8080, ()=>{ console.log('\x1b[32mEdgeBerry Asset Manager backend running on port '+8080+'\x1b[30m')});
-
-/*
- *  AWS IoT Client
- *
- *  Resources:
- *      getting started: AWS CLI:       https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
- *                       Credentials:   https://docs.aws.amazon.com/cli/latest/userguide/cli-authentication-user.html
- *                       
- * 
- *      credentials:     https://docs.aws.amazon.com/rekognition/latest/dg/setup-awscli-sdk.html
- *                       https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/getting-started-nodejs.html#getting-started-nodejs-credentials
- *                       https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html
- * 
- *      SDK info (!):    https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/iot/
- *                       https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/iot-data-plane/
- *                       https://docs.aws.amazon.com/iot/latest/apireference/API_Operations.html
- */
-
-
-const iotClientConfig = {
-    region: 'eu-north-1'
-}
-
-const AWSIoTClient = new IoTClient( iotClientConfig );
-
-
-async function listThings(){
-    try{
-        const command = new ListThingsCommand( {maxResults:20} );
-        const response = await AWSIoTClient.send( command );
-        console.log(response);
-    }
-    catch(err){
-        console.log(err)
-    }
-}
-listThings();
---
+++ src/routes/things.ts
@@ -0,0 +1,69 @@
+/*
+ *  Things routes (AWS IoT Core)
+ *
+ *  Resources:
+ *      getting started: AWS CLI:       https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+ *                       Credentials:   https://docs.aws.amazon.com/cli/latest/userguide/cli-authentication-user.html
+ *                       
+ * 
+ *      credentials:     https://docs.aws.amazon.com/rekognition/latest/dg/setup-awscli-sdk.html
+ *                       https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/getting-started-nodejs.html#getting-started-nodejs-credentials
+ *                       https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html
+ * 
+ *      SDK info (!):    https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/iot/
+ *                       https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/iot-data-plane/
+ *                       https://docs.aws.amazon.com/iot/latest/apireference/API_Operations.html
+ * 
+ *  required environment variables:
+ *  AWS_ACCESS_KEY_ID
+ *  AWS_SECRET_ACCESS_KEY
+ *  
+ */
+import { IoTClient, ListThingsCommand } from '@aws-sdk/client-iot';
+import { GetThingShadowCommand, IoTDataPlaneClient } from '@aws-sdk/client-iot-data-plane';
+
+import { Router } from "express";
+const router = Router();
+
+const iotClientConfig = {
+    region: 'eu-north-1'
+}
+
+const AWSIoTClient = new IoTClient( iotClientConfig );
+const AWSDataPlaneClient = new IoTDataPlaneClient( iotClientConfig );
+
+
+
+/* Get list of all things */
+router.get('/list', async(req:any, res:any)=>{
+    try{
+        var command = new ListThingsCommand( {maxResults:20} );
+        var response = await AWSIoTClient.send( command );
+        return res.send( response.things );
+    }
+    catch(err){
+        return res.status(500).send({message:err});
+    }
+});
+
+
+/* Get Shadow of a specific thing */
+router.get('/thingshadow', async(req:any, res:any)=>{
+    // Thing name in URL parameters
+    if( typeof req.query.thingName !== 'string') return res.status(400).send({message:"No thingName"});
+
+    try{
+        const command = new GetThingShadowCommand({thingName:req.query.thingName})
+        const response = await AWSDataPlaneClient.send( command );
+        
+        if( response.payload )
+        return res.send(JSON.parse( new TextDecoder().decode(response.payload)));
+
+        return res.status(500).send({message:"No payload"});
+    }
+    catch(err:any){
+        return res.status(500).send({message:err.name});
+    }
+});
+
+export default router;
