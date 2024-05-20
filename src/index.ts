d934d3356aa1a6147de60332c8d8d7d2874201f6
Tom Santens <tom.santens@gmail.com>
2024-05-20 12:06:15 +0200

    License: GPLv3

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 /*
- *  EdgeBerry Asset Manager
- *  An application for managing your EdgeBerry Assets in the cloud
+ *  Edgeberry Dashboard
+ *  An asset management platform for Edgeberry devices
  * 
- *  Copyright 2024, Sanne 'SpuQ' Santens
+ *  Copyright 2024 Sanne 'SpuQ' Santens
  * 
- *  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
- *  LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
- *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
  * 
- * 
- *  resources:
- *      https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create_deploy_nodejs.html
- *      https://github.com/marketplace/actions/beanstalk-deploy
- *      https://honesw.com/blog/how-to-see-an-ssl-certificate-https-on-an-elastic-beanstalk-environment-without-a-load-balancer
  */
 
 import express from 'express';
